e1f03d9e64811fa7772d59b7023b3bed5a1016ba
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-04-29 08:55:45 -0600

    Update Separate Word Button

--- Form1.cs
+++ Form1.cs
@@ -19,6 +19,8 @@ namespace Word_Separator
 
         private void btnSeparateWord_Click(object sender, EventArgs e)
         {
+            lblOutSeparatedWord.Text = "";
+
             string word = tboxEntryWord.Text.Trim();
 
             List<string> charList = new List<string>();
